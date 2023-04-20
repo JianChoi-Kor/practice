2f81151d4cdd1e21471ac4573ae3f474de1d4e64
JianChoi-Kor <wldks79@gmail.com>
2023-04-20 20:57:47 +0900

    @OneToOne 단방향, 외래 키가 주 테이블에 있는 경우

--- src/main/java/com/example/practice/n_pluse_1_issue/NPluse1Controller.java
+++ src/main/java/com/example/practice/n_pluse_1_issue/NPluse1Controller.java
@@ -4,12 +4,14 @@ import com.example.practice.n_pluse_1_issue.entity.Board;
 import com.example.practice.n_pluse_1_issue.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/npluse1")
 public class NPluse1Controller {
 
     private final BoardRepository boardRepository;
